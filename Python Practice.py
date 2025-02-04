f113649316bab2deae222946a7f1201a59e5621c
Abednego Andries <abeandries@icloud.com>
2025-02-04 10:26:19 +0700

    first commit (upload files)

---
+++ Python Practice.py
@@ -0,0 +1,43 @@
+# Soal 1
+Numbers = [41, 5, 1, 3, 89, 32]
+maks = 1
+# for i in Numbers:
+#     if i > maks:
+#         maks = i      
+# print(maks)
+
+# min = 1
+# for i in Numbers:
+#     if i < min:
+#         min = i
+# print(min)
+
+# Soal 2
+# comp = []
+# for i in range(1,8):
+#     if i % 3 == 0:
+#         continue
+#     comp.append(i**2)
+# print(comp)
+
+# Soal 3
+kalimat = 'Aku baru makan nasi terus aku mau makan mie nanti malam'
+result = kalimat.title().split()
+# print(splt2)
+kata = []
+# for i in result:
+#     if i not in kata:
+#         kata.append(i)
+# # print(kata)
+# for i in kata:
+#         print(f'Jumlah kata {i} ada {result.count(i)}')
+
+# Soal 4
+welcome_list = 'Selamat Datang di Pasar BuahList Menu:\n1. Menampilkan Daftar Buah'
+print(welcome_list)
+# while i in range(1,6):
+
+
+
+
+
